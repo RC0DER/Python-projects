3059f9845fa502c242f4f443a8b27a4e43011255
RCODER <rohit04.net@gmail.com>
2025-02-03 02:00:13 +0530

    problem solved

--- stone_paper_scissors.py
+++ stone_paper_scissors.py
@@ -4,34 +4,59 @@ list = ["rock", "paper", "scissors"]
 read = ""
 points_bot = 0
 points_you = 0
-points_play = int(input("Number of rounds: "))
+round = int(input("Number of rounds: "))
 i = 0
-while i < points_play:
+while i < round:
     choice_bot = random.choice(list)
+    print(choice_bot)
     print("1. Press 'r' for rock\n2. Press 'p' for paper\n3. Press 's' for scissors")
     choice = input("Enter your choice: ")
     if choice == "r":
-        choice = "rock"
+        if choice_bot == "rock":
+            print(" __________This round is draw__________")
+            print("______________________________________________")
+        elif choice_bot == "scissors":
+            points_you += 1
+            print(" __________You won this round: )________")
+            print("______________________________________________")
+        else :
+            points_bot += 1
+            print(" __________You loose this round :_(__________")
+            print("______________________________________________")
+        i += 1
+        continue
     elif choice == "p":
-        choice == "paper"
+        if choice_bot == "paper":
+            print(" __________This round is draw__________")
+            print("______________________________________________")
+        elif choice_bot == "rock":
+            points_you += 1
+            print(" __________You won this round: )________")
+            print("______________________________________________")
+        else :
+            points_bot += 1
+            print(" __________You loose this round :_(__________")
+            print("______________________________________________")
+        i += 1
+        continue
     elif choice == "s":
-        choice == "scissors"
+        if choice_bot == "scissors":
+            print(" __________This round is draw__________")
+            print("______________________________________________")
+        elif choice_bot == "paper":
+            points_you += 1
+            print(" __________You won this round: )________")
+            print("______________________________________________")
+        else :
+            points_bot += 1
+            print(" __________You loose this round :_(__________")
+            print("______________________________________________")
+        i += 1
+        continue
     else : 
         print(" __________Invalid input__________")
         print("______________________________________________")
         continue
-    if choice == choice_bot:
-        print(" __________This round is draw__________")
-        print("______________________________________________")
-    elif (choice == "paper" and choice_bot == "rock") or (choice == "scissors" and choice_bot == "paper") or (choice == "rock" and choice_bot == "scissors"):
-        points_you += 1
-        print(" __________You won this round: )________")
-        print("______________________________________________")
-    else :
-        points_bot += 1
-        print(" __________You loose this round :_(__________")
-        print("______________________________________________")
-    i += 1
 print("  __________   Your points =",points_you,"  __________")
 print("  __________ Computer points =",points_bot,"__________")
 if points_you > points_bot:
@@ -40,4 +65,5 @@ elif points_you < points_bot:
     print("  __________     You loose!!     __________")
 else :
     print("  __________        Draw!        __________")
+
 
